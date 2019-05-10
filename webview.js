cd2d5fecd326b8aa73f3d2e08fbab1bad9db234c
dweinber <daniel.weinberger@bitmovin.com>
2019-05-10 22:22:16 +0200

    Enable Android Messages' built-in dark mode based on the dark mode setting of Franz

--- webview.js
+++ webview.js
@@ -1,6 +1,12 @@
-module.exports = (Franz) => {
+module.exports = (Franz, settings) => {
   function getMessages() {
     Franz.setBadge(document.querySelectorAll('mws-conversation-list-item div.text-content.unread').length);
+  }
+
+  if (settings.isDarkModeEnabled) {
+    localStorage.setItem('dark_mode_enabled', 'true');
+  } else {
+    localStorage.setItem('dark_mode_enabled', 'false');
   }
 
   // check for new messages every second and update Franz badge
